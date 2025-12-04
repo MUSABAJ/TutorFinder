8d053f5f8be0bb46b53ccb85d47e7b7d7b8d654e
MUSABAJ <musabajebar@gmail.com>
2025-12-04 11:55:13 +0300

    static files collected throught whitenoise

--- _main/settings.py
+++ _main/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,7 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'os.path.join(base_DIR, "staticfiles")'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_URL = 'static/'
  
 # STATICFILES_DIRS = [
